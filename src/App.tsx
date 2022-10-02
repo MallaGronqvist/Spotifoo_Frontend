98808d61c139b1f656c0c720b3ea88deab4d3d0b
Malla <mallagronqvist@gmail.com>
2022-10-02 15:50:35 +0200

    Worked on fetching data from server.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Key, useEffect, useState } from "react";
-import Card from "./components/Card";
+import { useState, useEffect } from "react";
+import CardList from "./components/CardList";
+import React from "react";
 
 import "./styles/style.css";
 import NavigationBar from "./components/NavigationBar";
-import { AssertsThisTypePredicate } from "typescript";
 
 interface IState {
   songs: {
@@ -11,27 +11,19 @@ interface IState {
     title: string;
     artist: string;
     album: string;
-    pathTomusic: string;
-    pathToAlbum: string;
-  }[]
+    pathTomusic?: string;
+    pathToAlbum?: string;
+  }[];
 }
 
 export default function App() {
-  // Get songs somehow and give them to useState as a JSON
-  const [songs, setSongs] = useState<IState["songs"]>([])
+  const [songs, setSongs] = useState<IState["songs"]>([]);
 
   useEffect(() => {
-    fetch("localhost:8080/music")
+    fetch("http://localhost:8080/music/")
       .then((response) => response.json())
-      .then(setSongs);
+      .then((json) => setSongs(json));
   }, []);
-  /*
-if(songs) {
-  Cards = songs.map((item: { id: Key | null | undefined; title: string; })=> (
-    <Card key = {item.id} title = {item.title}/>
-    ));
-};
-*/
 
   return (
     <div className="App">
@@ -42,16 +34,14 @@ if(songs) {
         <section>
           <h2 id="Artist-title">Artist</h2>
           <div>
-            <Card title={"Test card"} />
+            <CardList songs={songs} />
           </div>
         </section>
         <section>
           <h2 id="Albums-title">Albums</h2>
-          <div></div>
         </section>
         <section>
           <h2 id="Genres-title">Genres</h2>
-          <div></div>
         </section>
       </main>
       <div id="MusicPlayer">HTML Music Player here</div>
---
+++ src/components/CardList.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+interface IProps {
+  songs: {
+    id: string;
+    title: string;
+    artist: string;
+    album: string;
+    pathTomusic?: string;
+    pathToAlbum?: string;
+  }[];
+}
+
+const CardList: React.FC<IProps> = ({ songs }) => {
+    let artists: Array<string> = [];
+
+    const getArtists = () => {
+      songs.map((song) =>{
+        if(! (artists.includes(song.artist))){
+            artists.push(song.artist);
+        }
+      }
+      ) 
+    }
+
+  const renderList = (): JSX.Element[] => {
+    return songs.map((song) => {
+      return (
+        <li key={song.id}>
+          <p className = "SongTitle">
+            {song.title}
+          </p>
+        </li>
+      );
+    });
+  };
+
+  const renderArtists = (): JSX.Element[] => {
+    getArtists();
+    return artists.map((artist) => {
+        return (
+          <li>
+            <p className = "SongTitle">
+              {artist}
+            </p>
+          </li>
+        );
+      });
+  }
+
+  return (
+
+  <ul>
+    {renderArtists()}
+  </ul>
+  ) 
+};
+
+export default CardList;
